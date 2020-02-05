2322035a0764c29070eae2d272e1cbb1564fbf5a
MarkusSagen <markus.johnson.93@gmail.com>
2020-02-05 11:46:33 +0100

    Updates

---
+++ precache-manifest.404eb6fbd4ed8335039a1e6fbc2f9962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e575560afbf7fe4bec38e5ee95ab325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa375c9f6b66f98c397",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64ec6b1260d2df86bbd2",
+    "url": "/static/js/2.3b3c269c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3b3c269c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa375c9f6b66f98c397",
+    "url": "/static/js/main.1d8034e8.chunk.js"
+  },
+  {
+    "revision": "1bbbfe343102f63c214f",
+    "url": "/static/js/runtime-main.976b912e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
